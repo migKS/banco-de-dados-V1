61bbc0c60f0f9a34863aa1e7b8bbb1fc326b4e67
migKS <miguel.kampa.souza@escola.pr.gov.br>
2025-10-03 11:18:22 -0300

    supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "url";
+const SUPABASE_URL = "https://zxljzsdiyorzdgwilyxd.supabase.co";
 const SUPABASE_ANON_KEY = "ANON";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
